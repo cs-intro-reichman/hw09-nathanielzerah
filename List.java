729d34fbe2af441ceaad6e1f2f8051082572025f
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-02-17 16:29:47 +0200

    List.java

--- List.java
+++ List.java
@@ -1,132 +1,113 @@
 public class List {
+
     private Node first;
     private int size;
 
+    public List() {
+        first = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public CharData getFirst() {
+        return first == null ? null : first.data;
+    }
+
     public void addFirst(char chr) {
-        CharData newCharData = new CharData(chr);
-
-        Node newNode = new Node(newCharData, first);
+        Node newNode = new Node(new CharData(chr), first);
         first = newNode;
-
         size++;
     }
-
+    
     public String toString() {
-        StringBuilder result = new StringBuilder("[");
+        StringBuilder builder = new StringBuilder("[");
         Node current = first;
-
         while (current != null) {
-            result.append(current.cp.toString());
-
+            builder.append(current.data.getChar());
             if (current.next != null) {
-                result.append(", ");
+                builder.append(", ");
             }
-
             current = current.next;
         }
-
-        result.append("]");
-        return result.toString();
+        builder.append("]");
+        return builder.toString();
     }
-
 
     public int indexOf(char chr) {
         Node current = first;
         int index = 0;
-
         while (current != null) {
-            if (current.cp.getChar() == chr) {
+            if (current.data.getChar() == chr) {
                 return index;
             }
-
             current = current.next;
             index++;
         }
-
         return -1;
     }
 
     public void update(char chr) {
         Node current = first;
-
         while (current != null) {
-            if (current.cp.getChar() == chr) {
-                current.cp.incrementCounter();
+            if (current.data.getChar() == chr) {
+                current.data.incrementCount();
                 return;
             }
-
             current = current.next;
         }
-
- 
         addFirst(chr);
     }
 
-
     public boolean remove(char chr) {
+        if (first == null) return false;
+        if (first.data.getChar() == chr) {
+            first = first.next;
+            size--;
+            return true;
+        }
+
         Node current = first;
-        Node previous = null;
-
-        while (current != null) {
-            if (current.cp.getChar() == chr) {
-                if (previous != null) {
- 
-                    previous.next = current.next;
-                } else {
-
-                    first = current.next;
-                }
-
+        while (current.next != null) {
+            if (current.next.data.getChar() == chr) {
+                current.next = current.next.next;
                 size--;
-
                 return true;
             }
-
-            previous = current;
             current = current.next;
         }
 
         return false;
     }
 
-
     public CharData get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
-
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
         Node current = first;
-        int currentIndex = 0;
-
-        while (currentIndex < index) {
+        for (int i = 0; i < index; i++) {
             current = current.next;
-            currentIndex++;
         }
-
-        return current.cp;
+        return current.data;
     }
 
     public CharData[] toArray() {
-	    CharData[] arr = new CharData[size];
-	    Node current = first;
-	    int i = 0;
+        CharData[] arr = new CharData[size];
+        Node current = first;
+        int i = 0;
         while (current != null) {
-    	    arr[i++]  = current.cp;
-    	    current = current.next;
+            arr[i++] = current.data;
+            current = current.next;
         }
         return arr;
     }
 
     public ListIterator listIterator(int index) {
-
-	    if (size == 0) return null;
-
-	    Node current = first;
-	    int i = 0;
-        while (i < index) {
+        if (size == 0) return null;
+        Node current = first;
+        for (int i = 0; i < index; i++) {
             current = current.next;
-            i++;
         }
-	    return new ListIterator(current);
+        return new ListIterator(current);
     }
 }
