9ebe08e700e4a01f81db2c237e9c11c17f54b2cc
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-02-17 19:41:44 +0200

    LanguageModel.java

--- LanguageModel.java
+++ LanguageModel.java
@@ -1,96 +1,133 @@
 import java.util.HashMap;
 import java.util.Random;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class LanguageModel {
-    HashMap<String, List> CharDataMap;
-    int windowLength;
-    private Random randomGenerator;
 
+    // The map of this model.
+    // Maps windows to lists of charachter data objects.
+    HashMap<String, List> CharDataMap;
+    
+    // The window length used in this model.
+    int windowLength;
+    
+    // The random number generator used by this model. 
+	private Random randomGenerator;
+
+    /** Constructs a language model with the given window length and a given
+     *  seed value. Generating texts from this model multiple times with the 
+     *  same seed value will produce the same random texts. Good for debugging. */
     public LanguageModel(int windowLength, int seed) {
         this.windowLength = windowLength;
         randomGenerator = new Random(seed);
-        CharDataMap = new HashMap<>();
+        CharDataMap = new HashMap<String, List>();
     }
 
+    /** Constructs a language model with the given window length.
+     * Generating texts from this model multiple times will produce
+     * different random texts. Good for production. */
     public LanguageModel(int windowLength) {
         this.windowLength = windowLength;
         randomGenerator = new Random();
-        CharDataMap = new HashMap<>();
+        CharDataMap = new HashMap<String, List>();
     }
 
-    public void train(String fileName) {
-        try {
-            FileInputStream fis = new FileInputStream(fileName);
-            Scanner scanner = new Scanner(fis);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                // Process the line for training
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    /** Builds a language model from the text in the given file (the corpus). */
+	public void train(String fileName) {
+		In in = new In(fileName); 
+        String text = in.readAll(); 
+        in.close();
+
+        for (int i = 0; i <= text.length() - windowLength; i++) {
+            String window = text.substring(i, i + windowLength);
+            char nextChar = i + windowLength < text.length() ? text.charAt(i + windowLength) : ' '; 
+
+            CharDataMap.putIfAbsent(window, new List());
+            List charList = CharDataMap.get(window);
+
+            charList.updateCharData(nextChar);
         }
-    }
+	}
 
-    public void calculateProbabilities(List probs) {
+    // Computes and sets the probabilities (p and cp fields) of all the
+	// characters in the given list. */
+	public void calculateProbabilities(List probs) {				
         double total = 0;
-     for (int i = 0; i < probs.getSize(); i++) {
-        CharData charData = probs.get(i);
-        total += charData.getCount();
-     }
-     double cumulativeProbability = 0;
-     for (int i = 0; i < probs.getSize(); i++) {
-        CharData charData = probs.get(i);
-        double probability = charData.getCount() / total;
-        cumulativeProbability += probability;
-        charData.setProbability(probability);
-        charData.setCumulativeProbability(cumulativeProbability);
+        
+        for (int i = 0; i < probs.size(); i++) {
+            CharData charData = (CharData) probs.get(i);
+            total += charData.frequency;
         }
-    }
+       
+        double cumulativeProbability = 0;
+        for (int i = 0; i < probs.size(); i++) {
+            CharData charData = (CharData) probs.get(i);
+            double probability = charData.frequency / total; 
+            cumulativeProbability += probability; 
+            charData.probability = probability; 
+            charData.cumulativeProbability = cumulativeProbability; 
+        }
+	}
 
-    public char getRandomChar(List probs) {
-        double randomValue = randomGenerator.nextDouble();
-        for (int i = 0; i < probs.getSize(); i++) {
-            CharData charData = probs.get(i);
-            if (randomValue <= charData.getCumulativeProbability()) {
-                return charData.getChar();
+    // Returns a random character from the given probabilities list.
+	public char getRandomChar(List probs) {
+        if (!CharDataMap.containsKey(window)) {
+            return ' '; 
+        }
+        List probs = CharDataMap.get(window);
+        double rand = Math.random();
+        double cumulativeProbability = 0;
+    
+        for (int i = 0; i < probs.size(); i++) {
+            CharData charData = (CharData) probs.get(i);
+            cumulativeProbability += charData.probability;
+            if (rand <= cumulativeProbability) {
+                return charData.character; 
             }
         }
+    
         return ' ';
-    }
+	}
 
-    public String generate(String initialText, int textLength) {
+    /**
+	 * Generates a random text, based on the probabilities that were learned during training. 
+	 * @param initialText - text to start with. If initialText's last substring of size numberOfLetters
+	 * doesn't appear as a key in Map, we generate no text and return only the initial text. 
+	 * @param numberOfLetters - the size of text to generate
+	 * @return the generated text
+	 */
+	public String generate(String initialText, int textLength) {
         StringBuilder generatedText = new StringBuilder(initialText);
-        for (int i = 0; i < textLength; i++) {
-            String window = generatedText.substring(Math.max(0, generatedText.length() - windowLength));
-            List probs = CharDataMap.get(window);
-            if (probs != null) {
-                char nextChar = getRandomChar(probs);
+            while (generatedText.length() < textLength + initialText.length()) {
+                String currentWindow = generatedText.substring(generatedText.length() - windowLength);
+                List probs = CharDataMap.get(currentWindow);
+                if (probs == null) {
+                    break;             }
+                char nextChar = getRandomChar(probs); 
                 generatedText.append(nextChar);
-            } else {
-                break;
             }
-        }
         return generatedText.toString();
-    }
+	}
 
-    public String toString() {
-        StringBuilder str = new StringBuilder();
-        for (String key : CharDataMap.keySet()) {
-            List keyProbs = CharDataMap.get(key);
-            str.append(key + " : " + keyProbs.toString() + "\n");
-        }
-        return str.toString();
-    }
+    /** Returns a string representing the map of this language model. */
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		for (String key : CharDataMap.keySet()) {
+			List keyProbs = CharDataMap.get(key);
+			str.append(key + " : " + keyProbs + "\n");
+		}
+		return str.toString();
+	}
 
     public static void main(String[] args) {
         LanguageModel model = new LanguageModel(3);
-        model.train("path/to/your/text/file.txt");
-        model.calculateProbabilities(); 
-        String generatedText = model.generate("The", 100);
+    
+        model.train("shakespeareinlove.txt");
+        model.train("originofspecies.txt");
+        
+        model.calculateProbabilities();
+        
+        String generatedText = model.generate("The ", 100); 
+        
         System.out.println(generatedText);
     }
 }
