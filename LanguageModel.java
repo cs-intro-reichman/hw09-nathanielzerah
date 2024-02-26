3f1b7f834cd560ea13a98dd56903199aa7597068
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-02-26 15:53:50 +0200

    LanguageModel.java

--- LanguageModel.java
+++ LanguageModel.java
@@ -95,7 +95,7 @@ public class LanguageModel {
 		StringBuilder str = new StringBuilder();
 		for (String key : CharDataMap.keySet()) {
 			List keyProbs = CharDataMap.get(key);
-			str.append(key + " : " + keyProbs + "\n");
+			str.append("(" + key + " : " + keyProbs + "\n" + ")");
 		}
 		return str.toString();
 	}
